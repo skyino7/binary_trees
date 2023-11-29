19db938dc29cb742f07e6b74856332a8ca292cbd
Seth Ketsi <christambassador12@gmail.com>
2023-11-29 20:58:36 +0000

    counts the leaves in a binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree
+ * Return: number of leaves
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t left, right;
+	
+	if (tree == NULL)
+		return (0);
+	
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+	
+	return (left + right);
+}
