c1b1454e16fafe5b8d5c9a74e75e428e346b5767
Seth Ketsi <christambassador12@gmail.com>
2023-11-29 12:25:37 +0000

    updated

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,12 +10,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 	
-	if (tree == NULL)
-		return (0);
-	
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-	
-	return ((MAX(left, right)) + (tree->left || tree->right ? 1 : 0));
+	if (tree != NULL)
+    {
+        left = binary_tree_height(tree->left);
+        right = binary_tree_height(tree->right);
+        
+        return ((MAX(left, right)) + (tree->left || tree->right ? 1 : 0));
+    }
+
+    return (0);
 
 }
