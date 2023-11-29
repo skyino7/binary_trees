bf354ae20d61a5139f5f3dc0a291fc863d25e646
Seth Ketsi <christambassador12@gmail.com>
2023-11-29 08:26:29 +0000

    updated

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,20 +12,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *left_chld = malloc(sizeof(binary_tree_t));
+	binary_tree_t *left_child = malloc(sizeof(binary_tree_t));
 
-	if (left_chld == NULL)
+	if (left_child == NULL)
 		return (NULL);
 
-	left_chld->n = value;
-	left_chld->parent = parent;
-	left_chld->left = parent->left;
-	left_chld->right = NULL;
+	left_child->n = value;
+	left_child->parent = parent;
+	left_child->left = parent->left;
+	left_child->right = NULL;
 
-	parent->left = left_chld;
+	parent->left = left_child;
 
-	if (left_chld->left != NULL)
-		left_chld->left->parent = left_chld;
+	if (left_child->left != NULL)
+		left_child->left->parent = left_child;
 
-	return (left_chld);
+	return (left_child);
 }
