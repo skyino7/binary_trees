f1e4b67d0cd22c4ad225a896b5e194a00ca8c37e
Seth Ketsi <christambassador12@gmail.com>
2023-11-29 10:17:10 +0000

    updated

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (right_child == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
 		right_child->right = parent->right;
 		parent->right->parent = right_child;
