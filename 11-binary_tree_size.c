c31aaf115dba537c1c7042237a608bec7551b351
Seth Ketsi <christambassador12@gmail.com>
2023-11-30 00:33:23 +0000

    updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,12 +9,12 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left, right;
-	
+
 	if (tree == NULL)
 		return (0);
-	
+
 	left = binary_tree_size(tree->left);
 	right = binary_tree_size(tree->right);
-	
+
 	return (left + right + 1);
 }
