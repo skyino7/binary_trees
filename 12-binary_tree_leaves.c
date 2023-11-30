8a97030ea4003fc6de2592f05e81323558c15d8a
Seth Ketsi <christambassador12@gmail.com>
2023-11-30 00:31:41 +0000

    updated

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,15 +9,15 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t left, right;
-	
+
 	if (tree == NULL)
 		return (0);
-	
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	
+
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
-	
+
 	return (left + right);
 }
